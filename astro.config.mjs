a8199415317b6af0c418cd5c7658fa4dd494a9b4
yuzhang-io <yu.zhang@wyss.harvard.edu>
2025-12-20 20:13:08 -0500

    optimize webp

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  // For GitHub user/organization pages:
+  site: "https://yuzhang-io.github.io",
+  base: "/",
+
   vite: {
     plugins: [tailwindcss()],
   },
